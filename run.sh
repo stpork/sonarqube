a06257801b2fcea5f7cd88358e57a76905baeb2b
stpork <stpork@gmail.com>
2017-11-29 11:05:18 +0200

    Revert Crowd changes

--- run.sh
+++ run.sh
@@ -11,10 +11,5 @@ exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
 -Dsonar.jdbc.username="$SONAR_JDBC_USERNAME" \
 -Dsonar.jdbc.password="$SONAR_JDBC_PASSWORD" \
 -Dsonar.jdbc.url="$SONAR_JDBC_URL" \
--Dsonar.security.realm="$SONAR_SECURITY_REALM" \
--Dsonar.security.localUser="$SONAR_SECURITY_LOCALUSERS"
--Dcrowd.application="$SONAR_CROWD_APPLICATION" \
--Dcrowd.password="$SONAR_CROWD_PASSWORD" \
--Dcrowd.url="$SONAR_CROWD_URL" \
 -Dsonar.web.javaAdditionalOpts="$SONAR_WEB_JVM_OPTS -Duser.home=${SONAR_HOME} -Djava.security.egd=file:/dev/./urandom" \
 "$@"
