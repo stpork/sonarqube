afc0fdf4406116621d6e853da77db67d08da2411
stpork <stpork@gmail.com>
2017-11-29 10:17:44 +0200

    Fixed quotes

--- run.sh
+++ run.sh
@@ -12,11 +12,9 @@ exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
 -Dsonar.jdbc.password="$SONAR_JDBC_PASSWORD" \
 -Dsonar.jdbc.url="$SONAR_JDBC_URL" \
 -Dsonar.security.realm="$SONAR_SECURITY_REALM" \
--Dsonar.security.localUser=$SONAR_SECURITY_LOCALUSERS
+-Dsonar.security.localUser="$SONAR_SECURITY_LOCALUSERS"
 -Dcrowd.application="$SONAR_CROWD_APPLICATION" \
 -Dcrowd.password="$SONAR_CROWD_PASSWORD" \
 -Dcrowd.url="$SONAR_CROWD_URL" \
--Dsonar.web.javaAdditionalOpts="$SONAR_WEB_JVM_OPTS \
--Duser.home=${SONAR_HOME} \
--Djava.security.egd=file:/dev/./urandom" \
+-Dsonar.web.javaAdditionalOpts="$SONAR_WEB_JVM_OPTS -Duser.home=${SONAR_HOME} -Djava.security.egd=file:/dev/./urandom" \
 "$@"
