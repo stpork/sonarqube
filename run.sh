4a12f3a76b73949c6e73e8b889f18134fb8bfea6
stpork <stpork@gmail.com>
2017-11-29 10:16:36 +0200

    Added support for Crowd

--- run.sh
+++ run.sh
@@ -7,9 +7,16 @@ if [ "${1:0:1}" != '-' ]; then
 fi
 
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
-  -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONAR_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONAR_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONAR_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="$SONAR_WEB_JVM_OPTS -Duser.home=${SONAR_HOME} -Djava.security.egd=file:/dev/./urandom" \
-  "$@"
+-Dsonar.log.console=true \
+-Dsonar.jdbc.username="$SONAR_JDBC_USERNAME" \
+-Dsonar.jdbc.password="$SONAR_JDBC_PASSWORD" \
+-Dsonar.jdbc.url="$SONAR_JDBC_URL" \
+-Dsonar.security.realm="$SONAR_SECURITY_REALM" \
+-Dsonar.security.localUser=$SONAR_SECURITY_LOCALUSERS
+-Dcrowd.application="$SONAR_CROWD_APPLICATION" \
+-Dcrowd.password="$SONAR_CROWD_PASSWORD" \
+-Dcrowd.url="$SONAR_CROWD_URL" \
+-Dsonar.web.javaAdditionalOpts="$SONAR_WEB_JVM_OPTS \
+-Duser.home=${SONAR_HOME} \
+-Djava.security.egd=file:/dev/./urandom" \
+"$@"
